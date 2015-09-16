7dbe4f703c4b5fdb702121994f9d9070d123e916
Hayes, Jesse L <jhayes@gatech.edu>
2015-09-16 19:51:41 -0400

    update player class

--- src/main/java/game/core/Player.java
+++ src/main/java/game/core/Player.java
@@ -9,7 +9,6 @@ package game.core;
  */
 public class Player {
 
-<<<<<<< HEAD
     private String name;
     private String color;
     private Race race;
@@ -20,63 +19,25 @@ public class Player {
         this.race = race;
     }
 
-=======
     //Player Variables
 
-    private int numPlayers;
     private String name;
     private Race race;
     private Color color;
 
 
     public enum Race{MECTRON, GOLLUMER, PACKER, BONZOID, SPHEROID, FLAPPER, LEGGITE, HUMANOID}
-    public enum Color{RED, YELLOW, GREEN, BLUE}
 
-    public Player() {
-
-        name = null;
-        race = null;
-        color = null;
-    }
-
-    public int getNumPlayers() {
-        return numPlayers;
-    }
-
-    public void setNumPlayers(int num) {
-        numPlayers = num;
-    }
-
-
->>>>>>> master
     public String getName() {
         return name;
     }
 
-<<<<<<< HEAD
     public String getColor() {
         return color;
-=======
-    public void setName(String newName) {
-        name = newName;
->>>>>>> master
     }
 
     public Race getRace() {
         return race;
     }
-
-<<<<<<< HEAD
-=======
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color c) {
-        color = c;
-    }
-
-
->>>>>>> master
 
 }
