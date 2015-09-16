d355212d94d2fd8f900b8425e5e03170a3b2c8e9
Cody <ctrawick3@gatech.edu>
2015-09-15 22:39:31 -0400

    Adding Game Info Classes

---
+++ src/main/java/game/core/GameInfo.java
@@ -0,0 +1,12 @@
+package game.core;
+
+/**
+ * This class holds the information that's important for the game to run
+ * It should take in the game information from the Configuration screen
+ * then help players create their Player information
+ *
+ * @author The SpecialFX
+ * @versoin 1.0
+ */
+public class GameInfo {
+}
---
+++ src/main/java/game/core/Player.java
@@ -0,0 +1,11 @@
+package game.core;
+
+/**
+ * This class represents a player. This class will hold the important
+ * information about that player
+ *
+ * @author The SpecialFX
+ * @version 1.0
+ */
+public class Player {
+}
