df7842bc05a212b89345e127d1ece4bec59ae642
Cody <ctrawick3@gatech.edu>
2015-09-21 23:43:23 -0400

    Adding Map Controller functionality

--- src/main/java/game/controllers/MapScreenController.java
+++ src/main/java/game/controllers/MapScreenController.java
@@ -1,13 +1,26 @@
 package game.controllers;
 
-
 import game.core.Engine;
 import game.core.GameScreen;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MapScreenController implements GameScreen {
-    Engine gameEngine;
+
+    private Engine gameEngine;
+
+    @FXML
+    private ImageView row0col0;
+
+    @FXML
+    void setImage(ActionEvent event) {
+        row0col0.setImage(new Image("/BlankTile.jpg"));
+    }
 
     public void setEngine(Engine parent) {
         gameEngine = parent;
     }
+
 }
