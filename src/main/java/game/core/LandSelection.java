f9cf49d66aa4a26bd0320c4270ef44815d32932b
Cody <ctrawick3@gatech.edu>
2015-09-23 18:59:41 -0400

    Removed Print Text

--- src/main/java/game/core/LandSelection.java
+++ src/main/java/game/core/LandSelection.java
@@ -10,7 +10,7 @@ public class LandSelection implements GameLogic {
     private int roundNum = 0;
     private int playerNum = 0;
     private int passStreak = 0;
-    private GameScreen view;
+    private MapScreenController view;
 
 
     public LandSelection(Engine gameEngine) {
@@ -31,7 +31,6 @@ public class LandSelection implements GameLogic {
     }
 
     public void tileWasClicked(int row, int column) {
-        System.out.println("Tile was clicked");
         Tile clickedTile = gameEngine.game.getMyGameMap().getTile(row, column);
         if (row == 2 && column == 4) {
             passStreak++;
@@ -40,7 +39,7 @@ public class LandSelection implements GameLogic {
             if (clickedTile.getOwner() == null) {
                 passStreak =0;
                 clickedTile.setOwner(gameEngine.game.getCurPlayer());
-                ((MapScreenController) view).addTileElement("Owner", gameEngine.getGame().getCurPlayer().getColor(), row, column);
+                view.addTileElement("Owner", gameEngine.getGame().getCurPlayer().getColor(), row, column);
                 nextPlayer();
             }
         } else {
@@ -49,7 +48,7 @@ public class LandSelection implements GameLogic {
                     gameEngine.getGame().getCurPlayer().spendMoney(300);
                     passStreak = 0;
                     clickedTile.setOwner(gameEngine.game.getCurPlayer());
-                    ((MapScreenController) view).addTileElement("Owner", gameEngine.getGame().getCurPlayer().getColor(), row, column);
+                    view.addTileElement("Owner", gameEngine.getGame().getCurPlayer().getColor(), row, column);
                     nextPlayer();
                 }
             }
@@ -72,6 +71,6 @@ public class LandSelection implements GameLogic {
     }
 
     public void setView(GameScreen view) {
-        this.view = view;
+        this.view = (MapScreenController) view;
     }
 }
