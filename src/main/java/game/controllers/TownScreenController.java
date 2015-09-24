87d2bb996296b50b2c756014083ddecd13709d6d
Jarred Aultman <jarred.aultman@gmail.com>
2015-09-23 20:05:34 -0400

    Created town screen controller

---
+++ src/main/java/game/controllers/TownScreenController.java
@@ -0,0 +1,61 @@
+package game.controllers;
+
+
+import game.core.Engine;
+import game.core.GameScreen;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TownScreenController implements GameScreen {
+
+    private Engine gameEngine;
+
+    @FXML
+    private Button landOffice;
+
+    @FXML
+    private Button assayOffice;
+
+    @FXML
+    private Button store;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    private Button pub;
+
+    @FXML
+    void clickBack(ActionEvent event) {
+    }
+
+    @FXML
+    void clickPub(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickLandOffice(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickAssay(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickStore(ActionEvent event) {
+
+    }
+
+    public void setEngine(Engine newEngine) {
+        gameEngine = newEngine;
+    }
+
+    public void initializeScreen() {
+
+    }
+
+}
