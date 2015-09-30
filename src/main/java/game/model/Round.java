7682462f7a0bf5915e4779b358673bf3d1db4df7
Cody <ctrawick3@gatech.edu>
2015-09-30 19:01:09 -0400

    Started adding Round Logic

---
+++ src/main/java/game/model/Round.java
@@ -0,0 +1,9 @@
+package game.model;
+
+/**
+ * Created by Cody on 9/30/2015.
+ */
+public class Round {
+    private Player[] orderedPlayers;
+    private int currentPlayer;
+}
---
+++ src/main/java/game/view/interfaces/ITurnStart.java
@@ -0,0 +1,12 @@
+package game.view.interfaces;
+
+import game.view.GameScreen;
+
+/**
+ * Created by Cody on 9/30/2015.
+ */
+public interface ITurnStart extends GameScreen {
+    void setRoundText(String text);
+    void setPlayerName(String name);
+
+}
