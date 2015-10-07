922ff0061784066f2698bf4513f2cbbe4b2f49bd
Cody Trawick <ctrawick3@gatech.edu>
2015-10-07 18:59:44 -0400

    Initialized Store Values

--- src/main/java/game/view/interfaces/IStoreScreen.java
+++ src/main/java/game/view/interfaces/IStoreScreen.java
@@ -2,8 +2,11 @@ package game.view.interfaces;
 
 import game.view.GameScreen;
 
+import java.util.HashMap;
+
 /**
  * Created by jarredaultman on 10/7/15.
  */
 public interface IStoreScreen extends GameScreen {
+    void setStoreAmounts(HashMap<String, Integer> amounts);
 }
